abf8268b8374c555af86652203c3cde8f5c65e20
Bruna Fernandes <bsfernandes17@gmail.com>
2020-10-17 13:37:31 -0300

    Initial commit

---
+++ CONFIGURAR_USETHIS.R
@@ -0,0 +1,14 @@
+library(usethis)
+
+
+
+#se apresentar o git
+usethis::use_git_config(user.name = "Bruna Fernandes",
+                        user.email = "bsfernandes17@gmail.com")
+
+#editar R environment
+usethis::edit_r_environ()
+
+
+#
+usethis::browse_github_token()
